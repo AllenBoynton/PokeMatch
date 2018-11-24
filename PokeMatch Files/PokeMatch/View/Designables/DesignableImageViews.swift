988436178245269eb93f972fb3c30c12c0485e82
Allen Boynton <alboynton4@gmail.com>
2018-11-24 14:50:52 -0500

    Update DesignableImageViews.swift

--- PokeMatch Files/PokeMatch/View/Designables/DesignableImageViews.swift
+++ PokeMatch Files/PokeMatch/View/Designables/DesignableImageViews.swift
@@ -36,15 +36,30 @@ import UIKit
 }
 
 extension UIImage {
-    func imageWithInsets(insets: UIEdgeInsets) -> UIImage? {
+    func imageWithInsets(insetDimen: CGFloat) -> UIImage {
+        return imageWithInset(insets: UIEdgeInsets(top: insetDimen, left: insetDimen, bottom: insetDimen, right: insetDimen))
+    }
+    
+    func imageWithInset(insets: UIEdgeInsets) -> UIImage {
         UIGraphicsBeginImageContextWithOptions(
             CGSize(width: self.size.width + insets.left + insets.right,
                    height: self.size.height + insets.top + insets.bottom), false, self.scale)
-        let _ = UIGraphicsGetCurrentContext()
         let origin = CGPoint(x: insets.left, y: insets.top)
         self.draw(at: origin)
         let imageWithInsets = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        return imageWithInsets
+        return imageWithInsets!
     }
+    
+//    func imageWithInsets(insets: UIEdgeInsets) -> UIImage? {
+//        UIGraphicsBeginImageContextWithOptions(
+//            CGSize(width: self.size.width + insets.left + insets.right,
+//                   height: self.size.height + insets.top + insets.bottom), false, self.scale)
+//        let _ = UIGraphicsGetCurrentContext()
+//        let origin = CGPoint(x: insets.left, y: insets.top)
+//        self.draw(at: origin)
+//        let imageWithInsets = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//        return imageWithInsets
+//    }
 }
