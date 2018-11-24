fb7a9b1db20fd9f22a95b69be1f3a0b949dc5d9e
Allen Boynton <alboynton4@gmail.com>
2018-11-24 14:51:35 -0500

    Update Gifs.swift

--- PokeMatch Files/PokeMatch/Model/Gifs.swift
+++ PokeMatch Files/PokeMatch/Model/Gifs.swift
@@ -37,7 +37,7 @@ struct RandomGifs {
     private let gifImages:[UIImage]
     
     init() {
-        gifImages = [UIImage(gifName: "1.gif", levelOfIntegrity:0.25), UIImage(gifName: "2.gif", levelOfIntegrity:0.25), UIImage(gifName: "3.gif", levelOfIntegrity:0.25), UIImage(gifName: "4.gif", levelOfIntegrity:0.25), UIImage(gifName: "5.gif", levelOfIntegrity:0.25), UIImage(gifName: "6.gif", levelOfIntegrity:0.25), UIImage(gifName: "7.gif", levelOfIntegrity:0.25), UIImage(gifName: "8.gif", levelOfIntegrity:0.25), UIImage(gifName: "9.gif", levelOfIntegrity:0.25), UIImage(gifName: "10.gif", levelOfIntegrity:0.25)]
+        gifImages = [UIImage(gifName: "1.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.gif", levelOfIntegrity:0.15), UIImage(gifName: "3.gif", levelOfIntegrity:0.15), UIImage(gifName: "4.gif", levelOfIntegrity:0.15), UIImage(gifName: "5.gif", levelOfIntegrity:0.15), UIImage(gifName: "6.gif", levelOfIntegrity:0.15), UIImage(gifName: "7.gif", levelOfIntegrity:0.15), UIImage(gifName: "8.gif", levelOfIntegrity:0.15), UIImage(gifName: "9.gif", levelOfIntegrity:0.15), UIImage(gifName: "10.gif", levelOfIntegrity:0.15)]
     }
     
     func randomGif() -> UIImage {
