6e91a91b2cef16c03857a205407e129152465249
Allen Boynton <alboynton4@gmail.com>
2018-11-24 14:52:52 -0500

    Update AppDelegate.swift
    Changed background and foreground functions

--- PokeMatch Files/PokeMatch/AppDelegate/AppDelegate.swift
+++ PokeMatch Files/PokeMatch/AppDelegate/AppDelegate.swift
@@ -28,44 +28,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         GADMobileAds.configure(withApplicationID: "ca-app-pub-2292175261120907~2981949238")
         PurchaseManager.instance.fetchProducts()
         
-        //        if #available(iOS 10.0, *) {
-        //            // For iOS 10 display notification (sent via APNS)
-        //            UNUserNotificationCenter.current().delegate = self
-        //
-        //            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-        //            UNUserNotificationCenter.current().requestAuthorization(
-        //                options: authOptions,
-        //                completionHandler: {granted, error in
-        //
-        //                if granted {
-        //                    print("Notification access granted")
-        //                } else {
-        //                    print(error?.localizedDescription as Any)
-        //                }
-        //            })
-        //        } else {
-        //            let settings: UIUserNotificationSettings =
-        //                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-        //            application.registerUserNotificationSettings(settings)
-        //        }
-        //
-        //        application.registerForRemoteNotifications()
-        
-        // Asks permission to show notifications
-        //        func askToShowVisibleNotifications() {
-        //            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-        //            UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { ( granted, error) in
-        //                print(error?.localizedDescription as Any)
-        //            }
-        //        }
-        
         return true
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        bgMusic?.pause()
+        if musicIsOn {
+            bgMusic?.pause()
+        }
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -76,6 +47,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        if musicIsOn {
+            bgMusic?.play()
+        }
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
