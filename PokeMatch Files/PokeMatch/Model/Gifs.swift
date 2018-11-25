e15c31d401adaa6d8d180510339c400931640a71
Allen Boynton <alboynton4@gmail.com>
2018-11-25 14:54:34 -0500

    Added gif files

--- PokeMatch Files/PokeMatch/Model/Gifs.swift
+++ PokeMatch Files/PokeMatch/Model/Gifs.swift
@@ -34,10 +34,15 @@ extension PokeMemoryGame : SwiftyGifDelegate {
 }
 
 struct RandomGifs {
-    private let gifImages:[UIImage]
+    private let gifImages: [UIImage]
+    private let flyingGifs: [UIImage]
     
     init() {
-        gifImages = [UIImage(gifName: "1.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.gif", levelOfIntegrity:0.15), UIImage(gifName: "3.gif", levelOfIntegrity:0.15), UIImage(gifName: "4.gif", levelOfIntegrity:0.15), UIImage(gifName: "5.gif", levelOfIntegrity:0.15), UIImage(gifName: "6.gif", levelOfIntegrity:0.15), UIImage(gifName: "7.gif", levelOfIntegrity:0.15), UIImage(gifName: "8.gif", levelOfIntegrity:0.15), UIImage(gifName: "9.gif", levelOfIntegrity:0.15), UIImage(gifName: "10.gif", levelOfIntegrity:0.15)]
+        gifImages = [UIImage(gifName: "1.0.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.1.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.2.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.3.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.4.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.5.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.6.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.7.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.8.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.9.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.0.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.gif", levelOfIntegrity:0.15), UIImage(gifName: "3.gif", levelOfIntegrity:0.15), UIImage(gifName: "4.gif", levelOfIntegrity:0.15), UIImage(gifName: "5.gif", levelOfIntegrity:0.15), UIImage(gifName: "6.gif", levelOfIntegrity:0.15), UIImage(gifName: "7.gif", levelOfIntegrity:0.15), UIImage(gifName: "8.gif", levelOfIntegrity:0.15), UIImage(gifName: "9.gif", levelOfIntegrity:0.15), UIImage(gifName: "10.gif", levelOfIntegrity:0.15), UIImage(gifName: "11.gif", levelOfIntegrity:0.15), UIImage(gifName: "12.gif", levelOfIntegrity:0.15), UIImage(gifName: "13.gif", levelOfIntegrity:0.15), UIImage(gifName: "14.gif", levelOfIntegrity:0.15)
+        ]
+        
+        flyingGifs = [UIImage(gifName: "1.0.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.1.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.2.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.3.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.4.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.5.gif", levelOfIntegrity:0.15),UIImage(gifName: "1.6.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.7.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.8.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.9.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.0.gif", levelOfIntegrity:0.15)
+        ]
     }
     
     func randomGif() -> UIImage {
