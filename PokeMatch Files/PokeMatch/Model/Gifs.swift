f2a7ce60ba2ccf164bfe7be0c152db3925be9b5a
Allen Boynton <alboynton4@gmail.com>
2018-11-25 15:17:13 -0500

    Update Gifs.swift
    Added to gif DB

--- PokeMatch Files/PokeMatch/Model/Gifs.swift
+++ PokeMatch Files/PokeMatch/Model/Gifs.swift
@@ -33,12 +33,12 @@ extension PokeMemoryGame : SwiftyGifDelegate {
     }
 }
 
-struct RandomGifs {
+class RandomGifs {
     private let gifImages: [UIImage]
     private let flyingGifs: [UIImage]
     
     init() {
-        gifImages = [UIImage(gifName: "1.0.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.1.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.2.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.3.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.4.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.5.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.6.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.7.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.8.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.9.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.0.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.gif", levelOfIntegrity:0.15), UIImage(gifName: "3.gif", levelOfIntegrity:0.15), UIImage(gifName: "4.gif", levelOfIntegrity:0.15), UIImage(gifName: "5.gif", levelOfIntegrity:0.15), UIImage(gifName: "6.gif", levelOfIntegrity:0.15), UIImage(gifName: "7.gif", levelOfIntegrity:0.15), UIImage(gifName: "8.gif", levelOfIntegrity:0.15), UIImage(gifName: "9.gif", levelOfIntegrity:0.15), UIImage(gifName: "10.gif", levelOfIntegrity:0.15), UIImage(gifName: "11.gif", levelOfIntegrity:0.15), UIImage(gifName: "12.gif", levelOfIntegrity:0.15), UIImage(gifName: "13.gif", levelOfIntegrity:0.15), UIImage(gifName: "14.gif", levelOfIntegrity:0.15)
+        gifImages = [UIImage(gifName: "1.0.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.1.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.2.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.3.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.4.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.5.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.6.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.7.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.8.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.9.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.0.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.1.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.gif", levelOfIntegrity:0.15), UIImage(gifName: "3.gif", levelOfIntegrity:0.15), UIImage(gifName: "4.gif", levelOfIntegrity:0.15), UIImage(gifName: "5.gif", levelOfIntegrity:0.15), UIImage(gifName: "6.gif", levelOfIntegrity:0.15), UIImage(gifName: "7.gif", levelOfIntegrity:0.15), UIImage(gifName: "8.gif", levelOfIntegrity:0.15), UIImage(gifName: "9.gif", levelOfIntegrity:0.15), UIImage(gifName: "10.gif", levelOfIntegrity:0.15), UIImage(gifName: "11.gif", levelOfIntegrity:0.15), UIImage(gifName: "12.gif", levelOfIntegrity:0.15), UIImage(gifName: "13.gif", levelOfIntegrity:0.15), UIImage(gifName: "14.gif", levelOfIntegrity:0.15), UIImage(gifName: "15.gif", levelOfIntegrity:0.15), UIImage(gifName: "16.gif", levelOfIntegrity:0.15)
         ]
         
         flyingGifs = [UIImage(gifName: "1.0.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.1.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.2.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.3.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.4.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.5.gif", levelOfIntegrity:0.15),UIImage(gifName: "1.6.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.7.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.8.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.9.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.0.gif", levelOfIntegrity:0.15)
@@ -51,5 +51,13 @@ struct RandomGifs {
         let randomNumber = Int(unsignedRandomNumber)
         
         return gifImages[randomNumber]
+    }
+    
+    func randomFlyingGif() -> UIImage {
+        let unsignedArrayCount = UInt32(flyingGifs.count)
+        let unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
+        let randomNumber = Int(unsignedRandomNumber)
+        
+        return flyingGifs[randomNumber]
     }
 }
