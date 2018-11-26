16a56c604706040e0dc3bd0ba5989a551260e8e4
Allen Boynton <alboynton4@gmail.com>
2018-11-26 01:16:34 -0500

    Update Gifs.swift
    Removed unused gif functions

--- PokeMatch Files/PokeMatch/Model/Gifs.swift
+++ PokeMatch Files/PokeMatch/Model/Gifs.swift
@@ -10,38 +10,15 @@ import Foundation
 import SwiftyGif
 import UIKit
 
-extension PokeMemoryGame : SwiftyGifDelegate {
-    
-    func gifURLDidFinish(sender: UIImageView) {
-        print("gifURLDidFinish")
-    }
-    
-    func gifURLDidFail(sender: UIImageView) {
-        print("gifURLDidFail")
-    }
-    
-    func gifDidStart(sender: UIImageView) {
-        print("gifDidStart")
-    }
-    
-    func gifDidLoop(sender: UIImageView) {
-        print("gifDidLoop")
-    }
-    
-    func gifDidStop(sender: UIImageView) {
-        print("gifDidStop")
-    }
-}
-
 class RandomGifs {
     private let gifImages: [UIImage]
     private let flyingGifs: [UIImage]
     
     init() {
-        gifImages = [UIImage(gifName: "1.0.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.1.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.2.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.3.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.4.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.5.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.6.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.7.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.8.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.9.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.0.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.1.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.gif", levelOfIntegrity:0.15), UIImage(gifName: "3.gif", levelOfIntegrity:0.15), UIImage(gifName: "4.gif", levelOfIntegrity:0.15), UIImage(gifName: "5.gif", levelOfIntegrity:0.15), UIImage(gifName: "6.gif", levelOfIntegrity:0.15), UIImage(gifName: "7.gif", levelOfIntegrity:0.15), UIImage(gifName: "8.gif", levelOfIntegrity:0.15), UIImage(gifName: "9.gif", levelOfIntegrity:0.15), UIImage(gifName: "10.gif", levelOfIntegrity:0.15), UIImage(gifName: "11.gif", levelOfIntegrity:0.15), UIImage(gifName: "12.gif", levelOfIntegrity:0.15), UIImage(gifName: "13.gif", levelOfIntegrity:0.15), UIImage(gifName: "14.gif", levelOfIntegrity:0.15), UIImage(gifName: "15.gif", levelOfIntegrity:0.15), UIImage(gifName: "16.gif", levelOfIntegrity:0.15)
+        gifImages = [UIImage(gifName: "1.gif", levelOfIntegrity: 0.12), UIImage(gifName: "2.gif", levelOfIntegrity: 0.12), UIImage(gifName: "3.gif", levelOfIntegrity: 0.12), UIImage(gifName: "4.gif", levelOfIntegrity: 0.12), UIImage(gifName: "5.gif", levelOfIntegrity: 0.12), UIImage(gifName: "6.gif", levelOfIntegrity: 0.12), UIImage(gifName: "8.gif", levelOfIntegrity: 0.12), UIImage(gifName: "9.gif", levelOfIntegrity: 0.12), UIImage(gifName: "10.gif", levelOfIntegrity: 0.12), UIImage(gifName: "11.gif", levelOfIntegrity: 0.12), UIImage(gifName: "12.gif", levelOfIntegrity: 0.12), UIImage(gifName: "13.gif", levelOfIntegrity: 0.12)
         ]
         
-        flyingGifs = [UIImage(gifName: "1.0.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.1.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.2.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.3.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.4.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.5.gif", levelOfIntegrity:0.15),UIImage(gifName: "1.6.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.7.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.8.gif", levelOfIntegrity:0.15), UIImage(gifName: "1.9.gif", levelOfIntegrity:0.15), UIImage(gifName: "2.0.gif", levelOfIntegrity:0.15)
+        flyingGifs = [UIImage(gifName: "1.0.gif", levelOfIntegrity: 0.12), UIImage(gifName: "1.1.gif", levelOfIntegrity: 0.12), UIImage(gifName: "1.2.gif", levelOfIntegrity: 0.12), UIImage(gifName: "1.3.gif", levelOfIntegrity: 0.12), UIImage(gifName: "1.4.gif", levelOfIntegrity: 0.12), UIImage(gifName: "1.5.gif", levelOfIntegrity: 0.12), UIImage(gifName: "1.7.gif", levelOfIntegrity: 0.12), UIImage(gifName: "1.8.gif", levelOfIntegrity: 0.12), UIImage(gifName: "1.9.gif", levelOfIntegrity: 0.12), UIImage(gifName: "2.0.gif", levelOfIntegrity: 0.12), UIImage(gifName: "2.1.gif", levelOfIntegrity: 0.12), UIImage(gifName: "2.2.gif", levelOfIntegrity: 0.12), UIImage(gifName: "2.4.gif", levelOfIntegrity: 0.12), UIImage(gifName: "2.5.gif", levelOfIntegrity: 0.12), UIImage(gifName: "2.6.gif", levelOfIntegrity: 0.12), UIImage(gifName: "2.7.gif", levelOfIntegrity: 0.12), UIImage(gifName: "2.8.gif", levelOfIntegrity: 0.12), UIImage(gifName: "2.9.gif", levelOfIntegrity: 0.12), UIImage(gifName: "3.0.gif", levelOfIntegrity: 0.12), UIImage(gifName: "3.1.gif", levelOfIntegrity: 0.12), UIImage(gifName: "3.2.gif", levelOfIntegrity: 0.12), UIImage(gifName: "3.4.gif", levelOfIntegrity: 0.12), UIImage(gifName: "3.5.gif", levelOfIntegrity: 0.12), UIImage(gifName: "3.6.gif", levelOfIntegrity: 0.12), UIImage(gifName: "3.7.gif", levelOfIntegrity: 0.12)
         ]
     }
     
