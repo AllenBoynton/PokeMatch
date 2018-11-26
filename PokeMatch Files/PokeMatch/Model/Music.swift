cd51f3f8470c31d2935ed9471145d3c86d995862
Allen Boynton <alboynton4@gmail.com>
2018-11-26 01:16:57 -0500

    Update Music.swift
    Added audio files and functions

--- PokeMatch Files/PokeMatch/Model/Music.swift
+++ PokeMatch Files/PokeMatch/Model/Music.swift
@@ -11,15 +11,19 @@ import GoogleMobileAds
 
 // Global references
 var bgMusic: AVAudioPlayer?
-var winnerAudio: AVAudioPlayer?
+var bgMusic2: AVAudioPlayer?
+var winnerAudio1: AVAudioPlayer?
+var winnerAudio2: AVAudioPlayer?
+var winnerAudio3: AVAudioPlayer?
+var winnerAudioArray: [AVAudioPlayer]?
 var musicIsOn = true
 
 class Music {
     
     // MARK: Sound files
-    func startGameMusic() {
+    func startGameMusic(name: String) {
         // BG music
-        let url = URL.init(fileURLWithPath: Bundle.main.path(forResource: "music", ofType: "mp3")!)
+        let url = URL.init(fileURLWithPath: Bundle.main.path(forResource: name, ofType: "mp3")!)
         
         do {
             bgMusic = try AVAudioPlayer(contentsOf: url)
@@ -31,15 +35,57 @@ class Music {
         }
     }
     
-    func playWinnerAudio() {
+    func startGameMusic2() {
+        // Winning music
+        let url = URL.init(fileURLWithPath: Bundle.main.path(forResource: "90 Ending Theme", ofType: "mp3")!)
+        
+        do {
+            bgMusic2 = try AVAudioPlayer(contentsOf: url)
+            bgMusic2?.prepareToPlay()
+            bgMusic2?.play()
+            bgMusic2?.numberOfLoops = 0
+        } catch let error as NSError {
+            print("audioPlayer error \(error.localizedDescription)")
+        }
+    }
+    
+    func playWinnerAudio1() {
         // Winning music
         let url = URL.init(fileURLWithPath: Bundle.main.path(forResource: "fanfare", ofType: "mp3")!)
         
         do {
-            winnerAudio = try AVAudioPlayer(contentsOf: url)
-            winnerAudio?.prepareToPlay()
-            winnerAudio?.play()
-            winnerAudio?.numberOfLoops = 0
+            winnerAudio1 = try AVAudioPlayer(contentsOf: url)
+            winnerAudio1?.prepareToPlay()
+            winnerAudio1?.play()
+            winnerAudio1?.numberOfLoops = 0
+        } catch let error as NSError {
+            print("audioPlayer error \(error.localizedDescription)")
+        }
+    }
+    
+    func playWinnerAudio2() {
+        // Winning music
+        let url = URL.init(fileURLWithPath: Bundle.main.path(forResource: "42 Fanfare_ Jackpot", ofType: "mp3")!)
+        
+        do {
+            winnerAudio2 = try AVAudioPlayer(contentsOf: url)
+            winnerAudio2?.prepareToPlay()
+            winnerAudio2?.play()
+            winnerAudio2?.numberOfLoops = 0
+        } catch let error as NSError {
+            print("audioPlayer error \(error.localizedDescription)")
+        }
+    }
+    
+    func playWinnerAudio3() {
+        // Winning music
+        let url = URL.init(fileURLWithPath: Bundle.main.path(forResource: "54 Fanfare_ Pokémon Caught", ofType: "mp3")!)
+        
+        do {
+            winnerAudio3 = try AVAudioPlayer(contentsOf: url)
+            winnerAudio3?.prepareToPlay()
+            winnerAudio3?.play()
+            winnerAudio3?.numberOfLoops = 0
         } catch let error as NSError {
             print("audioPlayer error \(error.localizedDescription)")
         }
@@ -48,17 +94,17 @@ class Music {
 
 extension Music {
     // Pause sound during ads
-    func handleMuteMusic() {
+    func handleMuteMusic(clip: AVAudioPlayer?) {
         // Pause sound if on
         if musicIsOn {
             // pauses music
             GADMobileAds.sharedInstance().applicationMuted = true
-            bgMusic?.pause()
-            winnerAudio?.stop()
+            clip?.pause()
+            winnerAudio1?.stop()
             musicIsOn = false
             print("Audio muted")
         } else {
-            bgMusic?.play()
+            clip?.play()
             musicIsOn = true
             print("Audio playing")
         }
