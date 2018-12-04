7ea36b1ca74c2e9f38c142685ebe8e709f78ee7e
Allen Boynton <alboynton4@gmail.com>
2018-12-04 10:02:45 -0500

    Create HTMLString.swift

---
+++ PokeMatch Files/PokeMatch/Extensions/HTMLString.swift
@@ -0,0 +1,23 @@
+//
+//  HTMLString.swift
+//  PokeMatch
+//
+//  Created by Allen Boynton on 12/3/18.
+//  Copyright © 2018 Allen Boynton. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return NSAttributedString() }
+        do {
+            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
+            return NSAttributedString()
+        }
+    }
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
+}
