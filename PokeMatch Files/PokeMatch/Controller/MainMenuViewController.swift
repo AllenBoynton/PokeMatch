c41790b6c86ee5530a18c989398c734a5b78450b
Allen Boynton <alboynton4@gmail.com>
2019-01-19 17:39:47 -0500

    Moved Gifs

--- PokeMatch Files/PokeMatch/Controller/MainMenuViewController.swift
+++ PokeMatch Files/PokeMatch/Controller/MainMenuViewController.swift
@@ -88,6 +88,7 @@ class MainMenuViewController: UIViewController {
     func handleGifViews() {
         gifView.contentMode = .scaleAspectFit
         gifView.setGifImage(RandomGifs.init().randomFlyingGifHD(), manager: gifManager, loopCount: -1)
+//        gifView.setGifImage(RandomGifs.init().randomFlyingGifHD())
     }
     
     func handleMusicButtons() {
